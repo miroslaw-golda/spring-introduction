6584af8c206ca86c452167d9a93345f15eb75c7e
Miroslaw Golda <miroslaw.golda@allegro.com>
2025-10-28 18:52:12 +0100

    two more tests and name change

--- src/test/java/pl/umk/workshop/springintroduction/SpringIntroductionApplicationTests.java
+++ src/test/java/pl/umk/workshop/springintroduction/SpringIntroductionApplicationTests.java
@@ -17,6 +17,7 @@ import pl.umk.workshop.springintroduction.domain.UmkCloakroomRepository;
 import java.util.List;
 
 import static org.junit.jupiter.api.Assertions.assertEquals;
+import static org.junit.jupiter.api.Assertions.assertNotEquals;
 import static org.junit.jupiter.api.Assertions.assertTrue;
 import static pl.umk.workshop.springintroduction.domain.models.Item.JACKET;
 
@@ -131,6 +132,35 @@ class SpringIntroductionApplicationTests extends TestsBase {
         var deposits = umkCloakroomRepository.findAll();
 
         assertEquals(5050, deposits.stream().map(Deposit::depositId).reduce(Integer::sum).get());
+    }
+
+    // By default beans are singletons.
+    // Every time we request it from the context, we get the same instance.
+    @Test
+    void singletonScope() {
+        // when
+        var incrementalNumberManager1 = (DepositNumberManager) context.getBean("incrementalDepositNumberManager");
+        var incrementalNumberManager2 = (DepositNumberManager) context.getBean("incrementalDepositNumberManager");
+
+        // then
+        assertEquals(incrementalNumberManager1, incrementalNumberManager2);
+        assertEquals(1, incrementalNumberManager1.getNextFreeNumber());
+        assertEquals(2, incrementalNumberManager2.getNextFreeNumber());
+    }
+
+    // Let's see how we can create prototype scoped beans.
+    // So that every time we request it from the context, we get a new instance.
+    // TIP: @Scope
+    @Test
+    void prototypeScope() {
+        // when
+        var incrementalNumberManager1 = (DepositNumberManager) context.getBean("prototypeDepositNumberManager");
+        var incrementalNumberManager2 = (DepositNumberManager) context.getBean("prototypeDepositNumberManager");
+
+        // then
+        assertNotEquals(incrementalNumberManager1, incrementalNumberManager2);
+        assertEquals(1, incrementalNumberManager1.getNextFreeNumber());
+        assertEquals(1, incrementalNumberManager2.getNextFreeNumber());
     }
 
     // Extract max number to application configuration
